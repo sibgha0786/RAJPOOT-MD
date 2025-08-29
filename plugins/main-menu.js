d63e4153e0d94ee30fff44e7eb36565b6fdfa67f
sibgha0786 <manohunny477@gmail.com>
2025-08-30 04:12:22 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -344,14 +344,14 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/lom1ep.png' },
+                image: { url: config.MENU_IMAGE_URL || 'https://i.ibb.co/tPk2kjqK/temp.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363403870276377@newsletter',
+                        newsletterJid: '120363421154527865@newsletter',
                         newsletterName: config.BOT_NAME,
                         serverMessageId: 143
                     }
