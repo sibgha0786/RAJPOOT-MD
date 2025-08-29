d42db3e7854e45071da788ce866a0f80578d49e6
sibgha0786 <manohunny477@gmail.com>
2025-08-29 16:26:21 +0500

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -38,8 +38,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: "120363403870276377@newsletter",
-          newsletterName: "PROxABDULLAH-MD Official",
+          newsletterJid: "120363421154527865@newsletter",
+          newsletterName: "RAJPOOT-MD Official",
           serverMessageId: 143
         }
       }
