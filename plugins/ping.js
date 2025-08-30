8f266b6fefb1df71f5310787028b03b154bb3e31
sibgha0786 <manohunny477@gmail.com>
2025-08-30 07:36:29 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *_RAJPOOT-MD_ SPEED-UP----->: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *_(RAJPOOT-MD)_ SPEED-UP----->: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -69,7 +69,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '*PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*🔥 _RAJPOOT-MD_ SPEED-UP-----> : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*🔥 _(RAJPOOT-MD)_ SPEED-UP-----> : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
